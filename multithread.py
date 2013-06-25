2ff094a758be6aa788d3dd352ede28161c18a7fd
Pengkui Luo <pengkui.luo@gmail.com>
2013-06-24 19:44:23 -0700

    Further abstracted into reusable classes 'WorkerThread' and 'ThreadPoolChain'.

--- multithread.py
+++ multithread.py
@@ -12,24 +12,24 @@
 
 """
 __all__ = [
-    'GenericThread',
-    'async_chained',
+    'WorkerThread',
+    'ThreadPoolChain',
 ]
 print('Executing %s' %  __file__)
 
-import os, sys, time
 import Queue
 import threading
 
 
-class GenericThread (threading.Thread):
-
+class WorkerThread (threading.Thread):
+    """ A worker thread that gets data from an input queue, calls a specified
+        function, and puts the (indexed) result into an output queue.
+    """
     def __init__ (self, func, inque, outque):
         threading.Thread.__init__(self)
         self.func = func
         self.inque = inque
         self.outque = outque
-
     def run (self):
         while True:
             index, item = self.inque.get()  # blocking
@@ -38,66 +38,83 @@ class GenericThread (threading.Thread):
             self.inque.task_done()
 
 
-def async_chained (input_list, func_list, workers_list):
+class ThreadPoolChain (object):
+    """ Asynchronous worker-thread pools chained by queues.
+    """
+    def __init__ (self, *funcs_workers):
+        """ Constructor for ThreadPoolChain.
+            e.g. ThreadPoolchain( (func1, num1), (func2, num2) )
+            means num1 workers for func1, num2 workers for func2.
+        """
+        numpools = len(funcs_workers)
+        #self.pools = [[] for _ in xrange(numpools)]
+        self.queues = [Queue.Queue() for _ in xrange(numpools + 1)]
+        for i, (func, numworkers) in enumerate(funcs_workers):
+            for _ in range(numworkers):
+                t = WorkerThread (func, self.queues[i], self.queues[i+1])
+                t.daemon = True
+                t.start()
+                #self.pools[i].append(t)
 
-    numpools = len(func_list)
-    output_list = [None for _ in xrange(len(input_list))]
+    def feed (self, inputs):
+        """ Feeds a sequence of data into the input queue, and obtains a
+            sequence of outputs that match the input order.
+        """
+        numinputs = len(inputs)
+        outputs = [None for _ in xrange(numinputs)]
+        for index, item in enumerate(inputs):
+            self.queues[0].put((index, item))
 
-    queues = [Queue.Queue() for _ in xrange(numpools + 1)]
+        # manually join/sync on the final output queue
+        finished = set()
+        while len(finished) < len(outputs):
+            index, result = self.queues[-1].get()  # blocking
+            outputs[index] = result
+            finished.add(index)
+            self.queues[-1].task_done()
 
-    for index, item in enumerate(input_list):
-        queues[0].put((index, item))
-
-    for i_pool in range(numpools):
-        func = func_list[i_pool]
-        for j_worker in range(workers_list[i_pool]):
-            t = GenericThread(func, queues[i_pool], outque=queues[i_pool+1])
-            t.daemon = True
-            t.start()
-
-    # join/sync on the final output queue
-    finished_indices = set()
-    while len(finished_indices) < len(output_list):
-        index, result = queues[-1].get()  # blocking
-        output_list[index] = result
-        finished_indices.add(index)
-        queues[-1].task_done()
-
-    #for i_pool in range(numpools+1):
-    #    queues[i_pool].join()
-
-    return output_list
+        # maybe unnecessary
+        #for i in range(len(self.pools)+1):
+        #    self.queues[i].join()
+        return outputs
 
 
 if __name__ == '__main__':
-    # Usage example, abstracted from
-    # http://www.ibm.com/developerworks/aix/library/au-threadingpython/
 
-    import time
-    import urllib2
-    from BeautifulSoup import BeautifulSoup
+    def _example1():
+        # Usage example, abstracted from
+        # http://www.ibm.com/developerworks/aix/library/au-threadingpython/
+        import time
+        import urllib2
+        from BeautifulSoup import BeautifulSoup
 
-    t0 = time.time()
+        def urlchunk (host):
+            url = urllib2.urlopen(host)
+            chunk = url.read()
+            return chunk
 
-    def urlchunk (host):
-        url = urllib2.urlopen(host)
-        chunk = url.read()
-        return chunk
+        def datamine (chunk):
+            soup = BeautifulSoup(chunk)
+            return soup.findAll(['title'])
 
-    def datamine(chunk):
-        soup = BeautifulSoup(chunk)
-        return soup.findAll(['title'])
+        tpc = ThreadPoolChain (
+            (urlchunk, 4),
+            (datamine, 2)
+        )
 
+        t0 = time.time()
+        results =  tpc.feed([
+            "http://google.com",
+            "http://yahoo.com",
+            "http://google.com",
+            "http://amazon.com",
+            "http://ibm.com",
+            "http://apple.com",
+            "http://www.mit.edu",
+            "http://www.cs.umn.edu",
+        ])
+        for i, res in enumerate(results):
+            print i, res
+        print('Done. Time elapsed: %.2f.' % (time.time() - t0))
 
-    hosts = ["http://yahoo.com",
-             "http://google.com",
-             "http://amazon.com",
-             "http://ibm.com",
-             "http://apple.com"]
-    funcs = [urlchunk, datamine]
-    workers = [2, 2]
-    results = async_chained (hosts, func_list=funcs, workers_list=workers)
-    for i, res in enumerate(results):
-        print i, res
-
-    print('Done. Time elapsed: %.2f.' % (time.time() - t0))
+    _example1()
