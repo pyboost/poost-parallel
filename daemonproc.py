204b7be47edbb6d00ebb4bed9df12cf4c6f44ecd
Pengkui Luo <pengkui.luo@gmail.com>
2013-06-27 16:14:51 -0700

    daemonproc.py skeleton.

---
+++ daemonproc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013 Pengkui Luo <pengkui.luo@gmail.com>
+# Created 06/27/2013, updated 06/27/2013
+#
+""" Creating Daemon processes. (TODO)
+
+    Reference:
+    http://code.activestate.com/recipes/278731/
+"""
+__all__ = []
+print('Executing %s' %  __file__)
+
+import os, sys, time
+
+
+if __name__ == '__main__':
+    t0 = time.time()
+    print('Done. Time elapsed: %.2f.' % (time.time() - t0))
